f95bfc89b60d6ce1db785f453d687e896d2e1e33
xyq608074 <xyq608074@163.com>
2018-08-28 18:02:37 +0800

    回文数

---
+++ src/huiwenshu/HuiWenShu.java
@@ -0,0 +1,20 @@
+package huiwenshu;
+
+public class HuiWenShu {
+    public static void main(String[] args) {
+        huiwenshu();
+    }
+    private static void huiwenshu(){
+        for (int i=10000; i<100000; i++){
+            int ge=i%10;
+            int shi=i/10%10;
+            int qian=i/10/10/10%10;
+            int wan=i/10/10/10/10%10;
+
+            if (ge==wan && shi==qian){
+                System.out.println(i);
+            }
+
+        }
+    }
+}
