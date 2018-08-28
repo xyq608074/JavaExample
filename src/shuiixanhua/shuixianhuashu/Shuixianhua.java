a493de72283233944375435b001e8c332474d89b
xyq608074 <xyq608074@163.com>
2018-08-28 17:01:39 +0800

    水仙花数字

--- src/shuiixanhua/shuixianhuashu/Shuixianhua.java
+++ src/shuiixanhua/shuixianhuashu/Shuixianhua.java
@@ -1,11 +1,11 @@
-package shuiixanhua;
+package shuiixanhua.shuixianhuashu;
 
 public class Shuixianhua {
     public static void main(String[] args) {
         printFlower();
     }
 
-    public static void printFlower(){
+    private static void printFlower(){
         for (int i=100;i<1000;i++){
             int ge=i%10;
             int shi=i/10%10;
