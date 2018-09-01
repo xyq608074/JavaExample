90d427e316dd901887ea0033831993643878b51e
xyq608074 <xyq608074@163.com>
2018-09-01 08:36:13 +0800

    文本内容复制粘贴

--- src/wenjiancopy/FileCopy.java
+++ src/wenjiancopy/FileCopy.java
@@ -6,14 +6,20 @@ import java.io.IOException;
 
 public class FileCopy {
     public static void main(String[] args) throws IOException {
+        //读取文件对象
         FileReader fr=new FileReader("src/wenjiancopy/copy.txt");
-
+        //写入文件对象
         FileWriter fw=new FileWriter("src/wenjiancopy/paste.txt");
 
         int ch;
+        /**
+         * fr.read()读取文件中每一个字符
+         * 当读取的时为-1时说明文件读取完毕
+         * 所以读取不等于-1就要一直读取下去
+         */
         while ((ch=fr.read())!=-1){
             System.out.print((char)ch);
-            fw.write((char)ch);
+            fw.write(ch);
         }
 
         fw.flush();
