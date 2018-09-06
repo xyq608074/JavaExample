bd676755f76cab12e6ab9fb04e27f008fbcf2286
xyq608074 <xyq608074@163.com>
2018-09-06 11:17:39 +0800

    使用静态方法做工具类做判断最大值和最大值下标

---
+++ src/maxnum/InputNum.java
@@ -0,0 +1,21 @@
+package maxnum;
+
+import java.util.Scanner;
+
+public class InputNum {
+    public static void main(String[] args) {
+        int[]  num=new int[5];
+        Scanner sc=new Scanner(System.in);
+        for (int i=0;i<num.length;i++){
+            System.out.println("请输入第"+(i+1)+"个数: ");
+            num[i] =sc.nextInt();
+        }
+        //静态方法不用new
+        //调用静态方法使用对象名称.静态方法名就行
+
+        //调用静态方法判断数组最大值
+        System.out.println("最大的数字是: "+MaxNum.getMax(num));
+        //调用静态方法判断数组最大值下标
+        System.out.println("最大的数组下标是: "+MaxNum.getMaxIndex(num));
+    }
+}
---
+++ src/maxnum/MaxNum.java
@@ -0,0 +1,31 @@
+package maxnum;
+
+public class MaxNum {
+    //返回数组最大的数字
+    public static int getMax(int[] arr){
+        //定义一个参照值
+        int max=0;
+
+        for (int i=0; i<arr.length;i++){
+            if (max<arr[i]){
+                max=arr[i];
+            }
+        }
+        return max;
+    }
+
+    //返回数组最大值的下标
+    public static int getMaxIndex(int[] arr){
+        //定义一个参照值
+        int max=0;
+        int maxindex=0;
+
+        for (int i=0; i<arr.length;i++){
+            if (max<arr[i]){
+                max=arr[i];
+                maxindex=i;
+            }
+        }
+        return maxindex;
+    }
+}
