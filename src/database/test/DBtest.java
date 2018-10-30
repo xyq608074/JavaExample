c5b2486fcf68d255ca179c845fb42bf673092782
nodine <xyq608074@163.com>
2018-10-30 10:48:32 +0800

    数据库工具类 并且测试增删改查

--- src/database/test/DBtest.java
+++ src/database/test/DBtest.java
@@ -2,6 +2,7 @@ package database.test;
 
 import database.utils.DBUtils;
 import org.junit.Test;
+import sun.security.pkcs11.Secmod;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -9,6 +10,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DBtest {
+    /**
+     * 查询所有
+     */
     @Test
     public void select(){
         Connection conn=null;
@@ -30,6 +34,10 @@ public class DBtest {
             DBUtils.release(conn,pstmt,rs);
         }
     }
+
+    /**
+     * 查询一条
+     */
     @Test
     public void selectOne(){
         Connection conn=null;
@@ -49,6 +57,87 @@ public class DBtest {
             e.printStackTrace();
         }finally {
             DBUtils.release(conn,pstmt,rs);
+        }
+    }
+
+    /**
+     * 添加
+     */
+    @Test
+    public void insert(){
+        Connection conn=null;
+        PreparedStatement pstmt=null;
+
+        try {
+            conn = DBUtils.getConnection();
+            String sql="insert into admin values(null,?,?)";
+            pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,"张三");
+            pstmt.setString(2,"zhangsan");
+            int i = pstmt.executeUpdate();
+            if (i>0){
+                System.out.println("添加成功");
+            }else{
+                System.out.println("添加失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.release(conn,pstmt,null);
+        }
+    }
+
+    /**
+     * 修改
+     */
+    @Test
+    public void update(){
+        Connection conn=null;
+        PreparedStatement pstmt=null;
+
+        try {
+            conn=DBUtils.getConnection();
+            String sql="update admin set adminname=?,adminpwd=? where admin_id=?";
+            pstmt=conn.prepareStatement(sql);
+            pstmt.setString(1,"李四");
+            pstmt.setString(2,"lisi");
+            pstmt.setInt(3,3);
+            int i = pstmt.executeUpdate();
+            if (i>0){
+                System.out.println("修改成功");
+            }else{
+                System.out.println("修改失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.release(conn,pstmt,null);
+        }
+    }
+
+    /**
+     * 删除
+     */
+    @Test
+    public void delete(){
+        Connection conn=null;
+        PreparedStatement pstmt=null;
+
+        try {
+            conn=DBUtils.getConnection();
+            String sql="delete from admin where admin_id=?";
+            pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1,3);
+            int i = pstmt.executeUpdate();
+            if (i>0){
+                System.out.println("删除成功");
+            }else{
+                System.out.println("删除失败");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            DBUtils.release(conn,pstmt,null);
         }
     }
 }
