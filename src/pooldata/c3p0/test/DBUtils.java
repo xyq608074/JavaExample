af8daf198ea9ff86a491f4581bcb01f3aabbe9ae
nodine <xyq608074@163.com>
2018-11-06 14:20:55 +0800

    c3p0连接池+DBUtils

--- src/pooldata/c3p0/test/DBUtils.java
+++ src/pooldata/c3p0/test/DBUtils.java
@@ -4,6 +4,7 @@ package pooldata.c3p0.test;
 import org.apache.commons.dbutils.QueryRunner;
 import org.apache.commons.dbutils.handlers.BeanHandler;
 import org.apache.commons.dbutils.handlers.BeanListHandler;
+import org.apache.commons.dbutils.handlers.ScalarHandler;
 import org.junit.Test;
 import pooldata.c3p0.domain.User;
 import pooldata.c3p0.utils.C3P0Utils;
@@ -91,11 +92,54 @@ public class DBUtils {
             //编写sql语句
             String sql="select * from users";
             //执行查询操作
+            //BeanListHandler  用于接收一个结果集封装到javaBean里面  在封装到 List 集合中
             List<User> users = qr.query(sql, new BeanListHandler<User>(User.class));
 
             for (User user: users) {
                 System.out.println(user.getId()+"---"+user.getUsername()+"---"+user.getUserpwd()+"---"+user.getUseremail()+"---"+user.getUserbirthday());
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 根据id查询
+     */
+    @Test
+    public void userQueryById(){
+        try {
+            //创建核心类QueryRunner
+            QueryRunner qr=new QueryRunner(C3P0Utils.getDataSource());
+            //编写sql语句
+            String sql="select * from users where id=?";
+            //为占位符赋值
+            Object[] params={19};
+            //执行查询操作
+            //BeanHandler 将结果中第一条封装到javaBean中
+            User user = qr.query(sql, new BeanHandler<User>(User.class), params);
+            //显示数据
+            System.out.println(user.getId()+"---"+user.getUsername()+"---"+user.getUserpwd()+"---"+user.getUseremail()+"---"+user.getUserbirthday());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 查询数据库中有多少条记录
+     */
+    @Test
+    public void userQueryCount(){
+        try {
+            //创建核心类QueryRunner
+            QueryRunner qr=new QueryRunner(C3P0Utils.getDataSource());
+            //编写sql语句
+            String sql="select count(*) from users";
+            //执行查询操作
+            //BeanHandler 将结果中第一条封装到javaBean中
+            Object count = qr.query(sql, new ScalarHandler());
+            //显示数据
+            System.out.println(count);
         } catch (SQLException e) {
             e.printStackTrace();
         }
